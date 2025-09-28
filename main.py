06360801f992c60041cf4042a08c8e0bcf69614f
qbekun <73672498+qbekun@users.noreply.github.com>
2025-09-29 01:08:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ from serial.tools import list_ports
 
 os.system('title /')
 
+print(r"""⠀⠀⠀⠀⠀⠀⠀
+⠀⠈⢇⠀⢹⡄⠀⠀⠀⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⡀⠀⢀⡠⣶⠿⢿⠙⣆⠀⢀⡈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠸⡀⠀⠉⠳⣤⡤⢴⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⣶⡿⠋⢁⣠⣾⣿⠿⡇⠀⠳⡄⠘⢦⣄⣀⠀⠀⠀⠀⠀
+⠤⠀⠠⠃⠀⠀⠀⠹⠼⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⢯⣠⣶⠟⠋⠁⢻⠀⢻⡀⠀⢻⣆⠀⠳⡈⠉⠙⠷⠶⠴
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡄⠀⣻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⢻⠸⣟⠁⠀⠀⢀⣼⣇⠀⣇⠀⠠⢻⣇⠀⠹⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⡿⠋⠁⠀⡿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⡄⢸⠀⢸⢠⣠⠶⢻⢭⣿⡀⢹⠀⠀⡉⢟⣇⢰⣽⠖⠦⢤⣀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣴⡿⠛⠉⠀⠀⡀⠀⡇⡇⣀⠀⢠⠀⠀⠀⠀⠀⠀⠀⣽⠀⠀⠀⡇⣸⠀⠸⣿⡅⠀⡼⣼⠀⣷⣾⠀⠀⠙⣾⣿⠟⢸⣀⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⣞⣿⡷⠋⠀⠀⠀⠀⢴⡇⢰⠀⡇⢿⡔⣸⠀⠀⠀⠀⠀⠀⢰⡿⠀⠀⣰⡇⡟⠀⠀⠏⣧⡶⠃⠇⢰⣿⣏⠀⠀⠀⢈⣻⣄⡿⢦⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡽⢛⠀⠀⠀⠤⡄⢠⣾⣧⢸⣀⣧⣼⣧⣿⡆⠀⠀⠀⢀⣤⡟⡏⢀⣰⢟⡿⠁⠀⠀⢀⣀⣤⣴⣤⣸⣶⣹⡄⠀⠀⠈⠻⡄⠀⠀⠳⣄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠈⣆⠀⠀⠀⡄⠈⣿⢹⣸⠀⠉⠀⢷⠃⡃⢀⢀⡴⣻⠟⠸⠗⡿⠋⠾⠁⠀⠀⢀⣼⣿⡟⠟⣿⣿⠉⣧⣳⠀⠀⠀⠀⠙⣦⣀⠀⠙⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇⢹⡆⠀⠀⢿⡀⣿⠈⣿⠀⠀⠀⠈⠀⠷⢟⠉⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠋⣽⣿⠀⣿⡝⠀⢹⣿⡆⠀⠀⠀⠀⢸⣌⡏⠉⠁
+⠀⠀⠰⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠙⡤⣿⣷⡀⠸⣧⣿⠀⢈⣷⣦⣴⣿⣶⣤⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⠀⢟⡇⠀⠘⠓⣇⠀⠀⠀⠀⠘⡟⡾⠀⠀
+⠀⠀⠀⠀⠄⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⢿⡝⢦⡏⢻⣤⣾⣿⣿⣿⡛⠉⠉⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡟⠀⣸⠀⠘⢦⠀⢹⢠⠀⠀⢠⣦⠁⠰⠀⠀
+⠀⠀⠀⠀⡄⠀⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⡛⠀⢁⣼⣿⣿⠋⣩⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⠇⠀⠘⣆⠀⠀⠳⣼⢸⣀⣠⠟⠏⠀⠀⠀⠀
+⠀⠀⠀⣰⠃⠀⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⣴⣾⣿⣿⣿⣶⣿⡿⠛⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⡀⠀⣿⠿⣍⡁⠀⠀⠀⠀⠀⠀
+⠀⠀⢰⠏⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣟⠟⠃⠀⢘⣇⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠁⠀⠀⠉⠓⠦⣄⡀⠀⠀
+⠀⣰⠏⠀⠀⠀⠀⠀⠀⢳⠀⠀⠀⠰⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢌⡉⠒⠛⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠀⠑⠦⣀⠀⠀⠀⠀⠀⠀⠉⠓⠦
+⣰⡏⠀⠰⠀⠀⠀⠀⠀⠈⢇⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣳⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡜⠀⠀⠀⢠⠞⠀⠀⠀⠀⠀⠙⠳⢤⣄⣀⡀⠀⠀⠀
+⢱⠁⠀⢀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣈⣹⡉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠐⠋⠀⠀⢀⡴⠋⠳⣄⡀⠀⠀⠀⢀⡤⠤⣭⣟⣯⣗⣀⠀
+⠈⠀⠀⠈⡆⠀⠀⠀⠀⠀⠀⠈⢳⡔⢆⡀⠀⠈⠳⣤⡀⠀⣀⣠⡴⣖⠒⠋⠉⠈⠙⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠋⠀⠀⠀⠀⠉⣁⡤⠒⠛⠛⠒⢬⠑⢦⠘⡍⢹
+⢀⠆⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠈⣾⡦⣽⡲⣄⡀⠀⠉⠙⢧⡀⠈⠉⠛⠶⣦⣀⡀⠀⠈⠓⢤⡶⠶⠶⠶⠖⠒⠊⣿⠿⠿⠿⣷⡶⠖⠾⠟⢋⣉⡁⠀⠀⠀⠀⠀⠀⠑⣌⢑⣾⠗
+⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⢿⡄⠉⠀⠉⠓⠦⣀⡀⠉⠲⣄⠀⠀⠀⠈⠙⠛⠶⢦⣤⣉⣲⣶⠒⠒⠋⣿⠁⠀⠀⠀⣼⢿⠉⠉⠉⠉⠁⡟⠓⢦⡀⠀⠀⠀⠀⢸⢿⠋⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠨⢿⣆⠀⠀⠀⠀⠈⠙⠳⣤⠈⢓⣦⣀⠀⠀⠀⠀⠀⠈⠙⠓⠩⠿⣒⡤⠿⣆⣚⠛⠉⢷⡞⠀⠀⠀⠀⠈⠀⠀⠀⠙⢄⠀⠀⠀⡀⠄⠀⠀⠀
+""")
+
 def parse_hpp(url):
     resp = requests.get(url)
     resp.raise_for_status()
@@ -101,6 +126,20 @@ ReadProcessMemory.restype = wintypes.BOOL
 
 CloseHandle.argtypes = [wintypes.HANDLE]
 CloseHandle.restype = wintypes.BOOL
+
+mciSendString = ctypes.windll.winmm.mciSendStringA
+
+def play_mp3(path):
+    # otwórz plik
+    mciSendString(b'open "' + path.encode() + b'" type mpegvideo alias mp3', None, 0, None)
+    # odtwórz w tle
+    mciSendString(b'play mp3', None, 0, None)
+
+def stop_mp3():
+    mciSendString(b'stop mp3', None, 0, None)
+    mciSendString(b'close mp3', None, 0, None)
+
+play_mp3("nya.mp3")
 
 def find_pid_by_name(proc_name):
     name = proc_name.lower()
@@ -279,3 +318,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def polymorphic_function():
+    if random.choice([True, False]):
+        return lambda x: x + 1
+    else:
+        return lambda x: x * 2
+
+def metamorphic_function(data):
+    if random.choice([True, False]):
+        return sum(data)
+    else:
+        return max(data)
+
+def decrypt_string(encrypted: bytes) -> str:
+    key = 0x55
+    return ''.join(chr(b ^ key) for b in encrypted)
+
+offsets_hpp_encrypted = b'\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
+client_hpp_encrypted  = b'\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
+
+offsets_hpp = decrypt_string(offsets_hpp_encrypted)
+client_hpp  = decrypt_string(client_hpp_encrypted)
